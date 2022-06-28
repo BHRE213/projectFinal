b74ecdb7200e102a64e9613f43829fab0a37eeee
batool <batoolmershed212@hotmail.com>
2022-06-28 15:39:41 +0300

    Update contact-us.component.ts

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,13 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-
 import { Router } from '@angular/router';
-import { NgxSpinnerService } from 'ngx-spinner';
 import { ToastrService } from 'ngx-toastr';
-
-import { MatDialog } from '@angular/material/dialog';
-import { NgxSpinnerService } from 'ngx-spinner';
-
 import { ContactUsService } from '../services/contact-us.service';
 
 @Component({
@@ -28,7 +22,6 @@ export class ContactUsComponent implements OnInit {
     phonenumber:new FormControl(''),    
   })
 
-  constructor(private spinner : NgxSpinnerService,public contact:ContactUsService, private dialog: MatDialog) { }
 
 
   ngOnInit(): void {
@@ -41,17 +34,7 @@ export class ContactUsComponent implements OnInit {
     window.location.reload();
   }
 
-  CreateForm: FormGroup = new FormGroup({
-    email: new FormControl('', Validators.required),
-    title: new FormControl('', Validators.required),
-    feedback: new FormControl('', Validators.required),
-    name: new FormControl('', Validators.required),
-    phonenumber: new FormControl()
-  })
-create (){
 
-  this.contact.createcontact(this.CreateForm.value)
-}
 
 
 }
