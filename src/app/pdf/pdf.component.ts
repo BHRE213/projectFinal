841fefde6976b22ff84f1229458ed57e8a5a5f02
OWL99196OWL <hebah1961999@gmail.com>
2022-07-09 01:56:05 +0300

    m

--- src/app/pdf/pdf.component.ts
+++ src/app/pdf/pdf.component.ts
@@ -8,48 +8,13 @@ import { Component, OnInit , ElementRef, EventEmitter} from '@angular/core';
   styleUrls: ['./pdf.component.css']
 })
 export class PdfComponent implements OnInit {
-nameoffile:any=""
-text:any=""
-upload=document.getElementById('upload')
-outputt=document.getElementById('output')
 
-  constructor(public elementRef: ElementRef) { }
 
-  
+  constructor() { }
+
+
   ngOnInit(): void {
   }
-  changeListener($event:any) : void {
-    this.readThis($event.target);
-  }
-  complete :any = new EventEmitter();
-  readThis(inputValue: any) : void {
-    var file:File = inputValue.files[0]; 
-    var myReader:FileReader = new FileReader();
-
-    myReader.onloadend = function(e){
-      // you can perform an action with readed data here
-      console.log(myReader.result);
-    
-     
-        
-  
-     
-    }
-    
-    console.log(myReader.readAsText(file));
-   
-
-  }
-  resultSet:any; // dont need it 
-
- loadFileAsText($event:any){
-    var fileToLoad = $event.files[0];
-  
-    var fileReader = new FileReader();
-    fileReader.onload = function(){
-      fileReader.result
-    };
   
   
-  }
 }
